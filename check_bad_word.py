05c55aa4d7af27a0b800571124f5ae7c74a42d72
Dheeraj Agrawal <dheeraja486@gmail.com>
2017-05-25 12:56:19 +0530

    Added Checker to check word in the program

--- check_bad_word.py
+++ check_bad_word.py
@@ -3,4 +3,15 @@ def read_text():
 	contents_of_the_file = text.read()
 	print(contents_of_the_file)
 	text.close()
+	check_profanity(contents_of_the_file)
+def check_profanity(text_to_check):
+	connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+	output = connection.read()
+	connection.close()
+	if "true" in output:
+		print("Profanity Alert!!")
+	elif "false" in output:
+		print("This document has no curse word!")
+	else:
+		print("Could not scan the document properly")
 read_text()	
